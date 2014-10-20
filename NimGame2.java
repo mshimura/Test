f298bf4fe6204996c0c0acc3f9d74d1648f11805
mshimura15 <mshimura15@matts-mbp.punahou.edu>
2014-10-20 12:22:57 -1000

    Test2

---
+++ NimGame2.java
@@ -0,0 +1,21 @@
+public class NimGame2
+{
+  public RandomNim(int startnum);
+}
+public int play (int pieces);
+{
+  if ((pieces-1)%3) == 0
+  {
+  return 2;
+  }
+  else;
+  {
+  if ((pieces-2)%3) ==0;
+  
+  {
+  return 1;
+  }
+  }
+}
+   
+    
